539a47ad27afd26af4d74c0ce9790baa66a1d436
shawnoloye <shawnoloye@gmail.com>
2022-08-23 17:41:54 +0100

    last commit

--- task2/task2.js
+++ task2/task2.js
@@ -2,5 +2,6 @@ const array = [2, 45, 6, 32, 45, 6, 43, 9];
 
 let largNum = array[0]
 for(i=0;i<array.length;i++){
-    largNum 
+    largNum = largNum > array[i] ? largNum : array[i] 
 }
+alert(largNum);
