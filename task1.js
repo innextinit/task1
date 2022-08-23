9e3d64fd5f70265da8c7a06b33b8f2543654c756
shawnoloye <shawnoloye@gmail.com>
2022-08-23 16:29:03 +0100

    first commit

--- task1.js
+++ task1.js
@@ -4,4 +4,9 @@ for(i=0;i<+0;i++){
         newH1.innerHTML = `${i} is even`;
         document.body.appendChild(newH1)
     }
+    if(1%2 != 0){
+        const newH1 = document.createElement("h1");
+        newH1.innerHTML = `${i} is odd`;
+        document.body.appendChild(newH1)
+    }
 }
