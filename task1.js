6ce69bb8c210ccb78dbfd72740aa94f8ece00b78
shawnoloye <shawnoloye@gmail.com>
2022-08-23 16:23:33 +0100

    added tast1

---
+++ task1.js
@@ -0,0 +1,7 @@
+for(i=0;i<+0;i++){
+    if(1%2 == 0){
+        const newH1 = document.createElement("h1");
+        newH1.innerHTML = `${i} is even`;
+        document.body.appendChild(newH1)
+    }
+}
