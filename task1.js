016e92601c1308c36e8c07bd2b5ae0547aaef128
Innextinit <innextinit@gmail.com>
2022-08-24 18:10:09 +0100

    testing push

--- task1.js
+++ task1.js
@@ -1,12 +1,13 @@
-for(i=0;i<+0;i++){
-    if(1%2 == 0){
-        const newH1 = document.createElement("h1");
-        newH1.innerHTML = `${i} is even`;
-        document.body.appendChild(newH1)
-    }
-    if(1%2 != 0){
-        const newH1 = document.createElement("h1");
-        newH1.innerHTML = `${i} is odd`;
-        document.body.appendChild(newH1)
-    }
+for (i = 0; i < +0; i++) {
+  if (1 % 2 == 0) {
+    const newH1 = document.createElement("h1");
+    newH1.innerHTML = `${i} is even`;
+    document.body.appendChild(newH1);
+  }
+  console.log("pushing");
+  if (1 % 2 != 0) {
+    const newH1 = document.createElement("h1");
+    newH1.innerHTML = `${i} is odd`;
+    document.body.appendChild(newH1);
+  }
 }
